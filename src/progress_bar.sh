5eef0eb424b69d8ea6b98bb63f7b7d029e6270e7
tinker <nurislam.khalimov1014@gmail.com>
2023-04-20 23:16:02 +0300

    modified:   README.md 	modified:   src/progress_bar.sh

--- src/progress_bar.sh
+++ src/progress_bar.sh
@@ -12,11 +12,10 @@ progress_bar() {
   fi
 }
 
+# This func needed to fill background progress by spaces
 progress_print() {
   echo -ne "\r" >&2
   echo -n "$1"
-  # This part needed to fix background progress by spaces filling
-  # You can comment part below and see what you got
   let progress_length=70-$(echo $1 | wc -c)
   if (( $progress_length > 0 )); then
     spaces=$(head -c $progress_length < /dev/zero | tr '\0' ' ')
